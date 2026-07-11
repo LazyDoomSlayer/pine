1283e48873bb256c6338ebbbc8a30e5c3be48d60
Luka Jioshvili <lazydoomslayer@gmail.com>
2026-07-11 15:23:29 +0200

    feat: Add more fields for output for table.

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -7,8 +7,13 @@ namespace Pine\Repositories;
 final readonly class Repository
 {
     public function __construct(
-        public string $name,
-        public string $path,
+        public string  $name,
+        public string  $path,
+        public ?string $branch = null,
+        public ?int    $ahead = null,
+        public ?int    $behind = null,
+        public ?int    $lastCommitTimestamp = null,
+        public ?bool   $dirty = null,
     )
     {
     }
